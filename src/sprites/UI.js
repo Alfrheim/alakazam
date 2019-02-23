7400e1c8631feed99e50b29f5958afc04af496a7
Alfrheim <marc.badiac@gmail.com>
2019-02-23 15:57:49 +0100

    spellbook has text now

--- src/sprites/UI.js
+++ src/sprites/UI.js
@@ -1,6 +1,7 @@
 const DEFAULT_BOOK_X = 10;
 const DEFAULT_BOOK_Y = 530;
 let isMenuShowed = false;
+const todos = ["Find blue potion", "Get a spoon"];
 
 function createUI(displayGroup, container) {
     
@@ -11,11 +12,15 @@ function createUI(displayGroup, container) {
     bookUI.interactive = true;
     bookUI.on('pointerdown', () => {
         if (!isMenuShowed) {
-            let text = new PIXI.Text('This is a pixi text',{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'});
-    
             bookPage.parentGroup = displayGroup;
+            let todoTextY = 20;
+            todos.map( todo => new PIXI.Text(todo,{fontFamily : 'Arial', fontSize: 24, fill : 0xff1010, align : 'center'} ))
+                .forEach(todoText => {
+                    todoText.x = 120;
+                    todoText.y = todoTextY += 30;
+                    bookPage.addChild(todoText);
+                });
     
-            bookPage.addChild(text);
             container.addChild(bookPage);
         } else {
             container.removeChild(bookPage);
