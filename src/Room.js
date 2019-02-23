7ace942f8645acddfaa3bb0b6d9e3a09b1b12080
lblanch <laura.blancho@gmail.com>
2019-02-23 17:07:31 +0200

    Room class works with display group and main container

--- src/Room.js
+++ src/Room.js
@@ -1,9 +1,8 @@
 class Room {
     constructor(backgroundResource) {
         this.background = new PIXI.Sprite(PIXI.loader.resources[backgroundResource].texture);
-        this.backgound.interactive = true;
-        this.backgound.on('pointerdown', onClickWalk);
-        this.interactiveItems;
+        this.background.interactive = true;
+        this.interactiveItems = [];
     }
 
     addInteractiveItem(itemResource, posX, posY, itemDescription) {
@@ -11,7 +10,19 @@ class Room {
         item.x = posX;
         item.y = posY;
         item.description = itemDescription;
+        item.buttonMode = true;
+        item.interactive = true;
         this.interactiveItems.push(item);
+    }
+
+    addToContainer(displayGroup, mainContainer) {
+        this.background.parentGroup = displayGroup;
+        mainContainer.addChild(this.background);
+
+        this.interactiveItems.forEach(element => {
+            element.parentGroup = displayGroup;
+            mainContainer.addChild(element);
+        });
     }
 }
 export default Room;
--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ PIXI.loader
     .add("images/fireplace.png")
     .add("images/chest.png")
     .add("images/book.png")
+    .add("images/book-fireplace.png")
     .on("progress", loadProgressHandler)
     .load(setup);
 
@@ -34,9 +35,14 @@ function setup() {
     var mainContainer = new PIXI.Container();
     app.stage.addChild(mainContainer);
 
-    const backgound = new PIXI.Sprite(PIXI.loader.resources["images/fireplace.png"].texture);   //the background is resource, so we can call it as such
-    backgound.interactive = true;       //we indicate that we will interact with this sprite
-    backgound.on('pointerdown', onClickWalk);   //when click, walk. Function is here a bit down
+    const habitacio_1 = new Room("images/fireplace.png");
+    habitacio_1.background.on('pointerdown', onClickWalk);
+    habitacio_1.addInteractiveItem("images/chest.png", 500, 500, "This is a chest");
+    habitacio_1.addInteractiveItem("images/book-fireplace.png", 100, 100, "this is a book");
+
+    //const backgound = new PIXI.Sprite(PIXI.loader.resources["images/fireplace.png"].texture);   //the background is resource, so we can call it as such
+    //backgound.interactive = true;       //we indicate that we will interact with this sprite
+    //backgound.on('pointerdown', onClickWalk);   //when click, walk. Function is here a bit down
 
     var backgroundDisplayGroup = new PIXI.display.Group(-1, false);
     app.stage.addChild(new PIXI.display.Layer(backgroundDisplayGroup));
@@ -44,20 +50,17 @@ function setup() {
     var uiDisplayGroup = new PIXI.display.Group(1, false);
     app.stage.addChild(new PIXI.display.Layer(uiDisplayGroup));
 
+    habitacio_1.addToContainer(backgroundDisplayGroup, mainContainer);
+    
     //we now show here the background and items. Order matters
     createUI(app, uiDisplayGroup, mainContainer);
 
-    backgound.parentGroup = backgroundDisplayGroup;
-    mainContainer.addChild(backgound);
+    //backgound.parentGroup = backgroundDisplayGroup;
+    //mainContainer.addChild(backgound);
 
     wizard = new Wizard("images/wizard.json", backgroundDisplayGroup, mainContainer);
 
-    const chest = createChest(backgroundDisplayGroup, mainContainer);
-
-
-    //app.stage.hitArea = new PIXI.Rectangle(0, 0, app.renderer.width, app.renderer.height);
-    //app.stage.interactive = true;
-    //app.stage.on('pointerdown', onClickWalk);
+    //const chest = createChest(backgroundDisplayGroup, mainContainer);
 
     //we create the "clock" with delta value, that will refresh the stuff
     app.ticker.add(delta => gameLoop(delta));
@@ -69,6 +72,7 @@ function gameLoop(delta) {
 }
 
 function onClickWalk () {
+    console.log("onclickwalk");
     let click = app.renderer.plugins.interaction.mouse.global;
     clickX = click.x;
     wizard.walk(clickX);  
