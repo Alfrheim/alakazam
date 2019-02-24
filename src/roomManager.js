c5e954862b693a2fc2e92261d783bd75483cd095
Alfrheim <marc.badiac@gmail.com>
2019-02-24 14:22:24 +0100

    When we have all items and click the ouija the ouija scene is appearing

--- src/roomManager.js
+++ src/roomManager.js
@@ -1,6 +1,6 @@
 import Room from '@/Room';
 
-function createRooms(displayGroup, container) {
+function createRooms(displayGroup, container, scenes) {
 
     let room = new Room("images/fireplace.png", container, displayGroup);
     room.addInteractiveItem("images/chest.png", 500, 500, "This is a chest", 0);
@@ -9,7 +9,7 @@ function createRooms(displayGroup, container) {
     let room2 = new Room("images/livingroom.png", container, displayGroup);
     room2.addInteractiveItem("images/mug.png", 300, 200, "This is a mug", 3);
     room2.addInteractiveItem("images/chest.png", 100, 100, "this is a book", 4);
-    room2.addInteractiveItem("images/ouijawb_small.png", 500, 400, "We are still missing something...", -1, "ouija");
+    room2.addInteractiveItem("images/ouijawb_small.png", 500, 400, "We are still missing something...", -1, "ouija", scenes);
 
     let room3 = new Room("images/kitchen.png", container, displayGroup);
     room3.addInteractiveItem("images/mug.png", 300, 200, "This is a mug", 5);
