de696cd4a2e8806b25a706e8e14a9cea29f3be36
lblanch <laura.blancho@gmail.com>
2019-02-24 03:46:01 +0200

    game over when the timer runs out

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import Wizard from '@/sprites/Wizard';
 import Countdown from '@/Countdown';
 import createUI from '@/sprites/UI';
 import createRooms from '@/roomManager';
-//import GameState from '@/GameState';
 import '@/css/index.css'
 
 
@@ -38,13 +37,13 @@ PIXI.loader
     .add("images/book-page.png")
     .add('fonts/gullhorn.ttf')
     .on("progress", loadProgressHandler)
-    .load(menu);
+    .load(setup);
 
 function loadProgressHandler(resource) {
     console.log("progress: " + PIXI.loader.progress + "%");
 }
 
-function menu() {
+function setup() {
     app.stage = new PIXI.display.Stage();
 
     gameMenuScene = new PIXI.Container();
@@ -61,26 +60,47 @@ function menu() {
     gameMenuScene.addChild(menuNameText);
     gameMenuScene.addChild(menuStartText);
     gameMenuScene.interactive = true;
-    gameMenuScene.on('pointerdown', setupGame);
+    gameMenuScene.on('pointerdown', resetGame);
+
+    gameOverScene = new PIXI.Container();
+    let gameOverText = new PIXI.Text("Game Over",{fontFamily : 'Verdana', fontSize: 42, fill : 0xff1010, align : 'center', strokeThickness: 10} );
+    let tryAgainText = new PIXI.Text("Click/tap to try again",{fontFamily : 'Verdana', fontSize: 24, fill : 0xff1010, align : 'center', strokeThickness: 10} );
+    gameOverText.x = app.view.width/2;
+    gameOverText.y = app.view.height/2;
+    gameOverText.anchor.x = 0.5;
+    gameOverText.anchor.y = 0.5;
+    tryAgainText.x = app.view.width/2;
+    tryAgainText.y = app.view.height/2 + 100;
+    tryAgainText.anchor.x = 0.5;
+    tryAgainText.anchor.y = 0.5;
+    gameOverScene.addChild(gameOverText);
+    gameOverScene.addChild(tryAgainText);
+    gameOverScene.interactive = true;
+    gameOverScene.visible = false;
+    gameOverScene.on('pointerdown', resetGame);
+
     app.stage.addChild(gameMenuScene);
+    app.stage.addChild(gameOverScene);
 }
 
-function setupGame() {
+function resetGame() {
     console.log("setup");
     gameMenuScene.visible = false;
-    //app.stage = new PIXI.display.Stage();
+    gameOverScene.visible = false;
+    gameScene = new PIXI.Container();
+
     var mainContainer = new PIXI.Container();
-    app.stage.addChild(mainContainer);
+    gameScene.addChild(mainContainer);
 
     var backgroundDisplayGroup = new PIXI.display.Group(-1, false);
-    app.stage.addChild(new PIXI.display.Layer(backgroundDisplayGroup));
+    gameScene.addChild(new PIXI.display.Layer(backgroundDisplayGroup));
 
 
     var wizardDisplayGroup = new PIXI.display.Group(5, false);
-    app.stage.addChild(new PIXI.display.Layer(wizardDisplayGroup));
+    gameScene.addChild(new PIXI.display.Layer(wizardDisplayGroup));
 
     var uiDisplayGroup = new PIXI.display.Group(10, false);
-    app.stage.addChild(new PIXI.display.Layer(uiDisplayGroup));
+    gameScene.addChild(new PIXI.display.Layer(uiDisplayGroup));
 
     const room = createRooms(backgroundDisplayGroup, mainContainer);
     room.background.on('pointerdown', onClickWalk);
@@ -95,6 +115,7 @@ function setupGame() {
     countDown = new Countdown(uiDisplayGroup, mainContainer);
 
     //we create the "clock" with delta value, that will refresh the stuff
+    app.stage.addChild(gameScene);
     app.ticker.add(delta => gameLoop(delta));
 
 }
@@ -102,6 +123,10 @@ function setupGame() {
 function gameLoop(delta) {
     wizard.checkWizardWalk(clickX);
     countDown.refresh();
+    if (countDown.isOverTime()) {
+        gameScene.visible = false;
+        gameOverScene.visible = true;
+    }
 }
 
 function onClickWalk () {
@@ -110,5 +135,3 @@ function onClickWalk () {
     clickX = click.x;
     wizard.walk(clickX);  
 }
-
-export {setupGame};
